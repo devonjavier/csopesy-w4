ec57122427a17fbc49a0ff50042ed00fdef1d105
devonjavier <devonjarekjavier@gmail.com>
2025-05-31 21:38:28 +0800

    feat: updated seatwork, cpu outisde of class

--- seatwork.h
+++ seatwork.h
@@ -3,20 +3,25 @@
 #include <vector>
 #include <unordered_map>
 
+// CPU ITSELF
+float cpu_util;                             // CPU Utilization Percentage SHOULD BE BASED AND COMPUTED ON PROCESSES
+int cores_used;                             // no. of cores used
+int cores_total;                            // total no. of cores
+
 // a process changes states
 class Process {
     // # 1 
     int pid;                                    // Process ID
-    float cpu_util;                             // CPU Utilization Percentage                            
     std::string p_name;                         // Process name
     std::time_t p_time;                         // Process Time (Time Created)
     int p_counter;                              // Program Counter (Current Instruction)
-    int cores_used;                             // no. of cores used
-    int corse_total;                            // total no. of cores
     std::vector<std::string> p_instructions;    // instruction history
 
+    int p_cores_used;                             // no. of cores used
+    int p_cores_total;                            // total no. of cores
+
     Process(std::string name, int id, int cores) 
-        : p_name(name), pid(id), cores_used(cores), corse_total(cores), p_counter(0), cpu_util(0.0) {
+        : p_name(name), pid(id), p_cores_used(cores), p_cores_total(cores), p_counter(0) {
         p_time = std::time(nullptr);
     }
 
@@ -61,16 +66,17 @@ class Process {
 
 class ProcessManager{
     private:
+    // #3 this stores multiple processes
     std::unordered_map<int, Process> processMap;
 
     public:
 
-    // #3 & 4
+    // #3 & 4 adds processes to the unordered map
     void add_process(const Process& process) {
         // processes.insert({process.pid, process});  
     }
 
-    // #4
+    // #4 
     Process* getProcessByPid(int pid) {
         // auto it = processMap.find(pid);
         // if (it != processMap.end()) {
